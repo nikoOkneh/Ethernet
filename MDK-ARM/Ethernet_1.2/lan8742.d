83c0ba0db3430a231760d0be2adc243f52eab6c2
n.shevchenko <n.shevchenko@rtc.ru>
2025-07-25 16:10:26 +0300

    init project

---
+++ MDK-ARM/Ethernet_1.2/lan8742.d
@@ -0,0 +1,3 @@
+ethernet_1.2\lan8742.o: ../Drivers/BSP/Components/lan8742/lan8742.c
+ethernet_1.2\lan8742.o: ../Drivers/BSP/Components/lan8742/lan8742.h
+ethernet_1.2\lan8742.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MDK-ARM/Ethernet_1.2/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+ethernet_1.2\startup_stm32f407xx.o: startup_stm32f407xx.s
